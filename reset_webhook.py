0419ef149427efc5bacb5d9005880ff59144dac0
Capyk-cap <vladelecfermy@gmail.com>
2026-06-10 03:46:10 +0300

    Delete reset_webhook.py

--- reset_webhook.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-TOKEN = "8654250066:AAHZ6LBm5qjkgVHYk415fM-LMP-FKX84psc"
-
-url = f"https://api.telegram.org/bot{TOKEN}/deleteWebhook?drop_pending_updates=True"
-response = requests.get(url)
-print(response.json())
